3da33ac36ae66bc199d28ebc8fa3a2ce3976603b
Tyeryekhova Iryna <iro4kaforyou@gmail.com>
2023-07-20 14:23:18 +0300

    add EncodingFilter, Index file, css file, resources.properties and resources_ua.properties

--- src/main/java/org/example/сonfig/MySpringMvcDispatcherServletInitializer.java
+++ src/main/java/org/example/сonfig/MySpringMvcDispatcherServletInitializer.java
@@ -1,15 +1,16 @@
 package org.example.сonfig;
 
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
+import javax.servlet.*;
+import java.util.EnumSet;
 
 public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class[0];
+        return new Class[]{SpringConfig.class};
     }
 
     @Override
@@ -23,11 +24,50 @@ public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationC
     }
     @Override
     public void onStartup(ServletContext aServletContext) throws ServletException {
+//        addEncodingFilter(aServletContext);//    додала з інтернету
+
         super.onStartup(aServletContext);
+//        aServletContext.addFilter("characterSetFilter", new CharacterSetFilter()).addMappingForUrlPatterns(null, true, "/*");
+        addEncodingFilter(aServletContext);
         registerHiddenFieldFilter(aServletContext);
     }
     private void registerHiddenFieldFilter(ServletContext aServletContext) {
         aServletContext.addFilter("hiddenHttpMethodFilter", new HiddenHttpMethodFilter())
                 .addMappingForUrlPatterns(null, true, "/*");
     }
+
+//    додала з інтернету
+    private void addEncodingFilter(ServletContext servletContext) {
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+
+        EnumSet<DispatcherType> dispatcherTypes
+                = EnumSet.of(DispatcherType.REQUEST, DispatcherType.FORWARD, DispatcherType.ERROR);
+
+        FilterRegistration.Dynamic characterEncoding
+                = servletContext.addFilter("characterEncoding", characterEncodingFilter);
+        characterEncoding.addMappingForUrlPatterns(dispatcherTypes, true, "/*");
+        characterEncoding.setAsyncSupported(true);
+    }
+
+    //    додала з інтернету
+//    @Override
+//    protected Filter[] getServletFilters() {
+//        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+//        characterEncodingFilter.setEncoding("UTF-8");
+//        characterEncodingFilter.setForceEncoding(true);
+//        return new Filter[] {characterEncodingFilter};
+//    }
+//    @Override
+//    protected Filter[] getServletFilters() {
+//        Filter[] filters;
+//        CharacterEncodingFilter encFilter;
+//        HiddenHttpMethodFilter httpMethodFilter = new HiddenHttpMethodFilter();
+//        encFilter = new CharacterEncodingFilter();
+//        encFilter.setEncoding("UTF-8");
+//        encFilter.setForceEncoding(true);
+//        filters = new Filter[] {httpMethodFilter, encFilter};
+//        return filters;
+//    }
 }
--- src/main/java/org/example/сonfig/SpringConfig.java
+++ src/main/java/org/example/сonfig/SpringConfig.java
@@ -2,19 +2,23 @@ package org.example.сonfig;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
 import javax.sql.DataSource;
+import java.util.Locale;
 import java.util.logging.Logger;
 
 @Configuration
@@ -30,6 +34,7 @@ public class SpringConfig implements WebMvcConfigurer {
     @Bean
     public SpringResourceTemplateResolver templateResolver() {
         SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setCharacterEncoding("UTF-8");
         templateResolver.setApplicationContext(applicationContext);
         templateResolver.setPrefix("/WEB-INF/views/");
         templateResolver.setSuffix(".html");
@@ -47,15 +52,16 @@ public class SpringConfig implements WebMvcConfigurer {
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
         ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+        resolver.setCharacterEncoding("UTF-8");
         resolver.setTemplateEngine(templateEngine());
         registry.viewResolver(resolver);
     }
+
 
 //    створюємо з'єднання через jdbcTemplate
     @Bean
     public DataSource dataSource(){
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
         dataSource.setUrl("jdbc:mysql://localhost:3306/paymentdb?useUnicode=yes;characterEncoding=UTF-8&serverTimezone=UTC&useSSL=false");
         dataSource.setUsername("root");
@@ -67,4 +73,45 @@ public class SpringConfig implements WebMvcConfigurer {
     public JdbcTemplate jdbcTemplate(){
         return new JdbcTemplate(dataSource());
     }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        //Назначает локаль по умончанию, которая используется когда к сесии не прикреплена никакая локаль.
+        //Если не назначать локаль по умолчанию, то локаль будет назначена согласно Accept-Language хэдера запроса
+        slr.setDefaultLocale(Locale.forLanguageTag("ua"));
+        return slr;
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+//        messageSource.setCacheSeconds(5); //refresh cache once per 5 sec
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setFallbackToSystemLocale(false);
+        messageSource.setBasenames("classpath:resources");
+        return messageSource;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/styles/css/stylesheet.css")
+                .addResourceLocations("classpath:/static/css/stylesheet.css");}
+//        registry.addResourceHandler("/images/**")
+//                .addResourceLocations("classpath:/static/images/");
+//        registry.addResourceHandler("/js/**")
+//                .addResourceLocations("classpath:/static/js/");
+//    }
 }
