d8f45fcbcf381bf0ad497c0641111671b016bc93
Tyeryekhova Iryna <iro4kaforyou@gmail.com>
2023-10-31 20:42:24 +0200

    add method adminCardMenuMakePost in AdminCardMenuController.java

--- src/main/java/org/example/controllers/AdminCardMenuController.java
+++ src/main/java/org/example/controllers/AdminCardMenuController.java
@@ -5,6 +5,7 @@ import org.example.dbServices.DBClientService;
 import org.example.models.AdminCardMenuService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.ServletException;
@@ -23,7 +24,12 @@ public class AdminCardMenuController {
     }
 
     @GetMapping
-    protected String adminCardMenuMake(HttpServletRequest request) throws ServletException, IOException {
+    protected String adminCardMenuMakeGet(HttpServletRequest request) throws ServletException, IOException {
        return new AdminCardMenuService().adminCardMenuMake(request, cardService, clientService);
+    }
+
+    @PostMapping
+    protected String adminCardMenuMakePost(HttpServletRequest request) throws ServletException, IOException {
+        return new AdminCardMenuService().adminCardMenuMake(request, cardService, clientService);
     }
 }
