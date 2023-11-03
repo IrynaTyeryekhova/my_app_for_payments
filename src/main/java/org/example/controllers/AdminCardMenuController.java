7dd08be9c6ce19455e2816c1fa95551af78738ed
Tyeryekhova Iryna <iro4kaforyou@gmail.com>
2023-11-03 20:48:28 +0200

    add GET and POST methods at AdminCardMenuController.java for requestsToUnlockCards

--- src/main/java/org/example/controllers/AdminCardMenuController.java
+++ src/main/java/org/example/controllers/AdminCardMenuController.java
@@ -2,15 +2,13 @@ package org.example.controllers;
 
 import org.example.dbServices.DBCardService;
 import org.example.dbServices.DBClientService;
-import org.example.models.AdminCardMenuService;
+import org.example.models.AdminUnlockCardsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
 
 @Component
 @RequestMapping("/adminCardsMenu")
@@ -23,13 +21,27 @@ public class AdminCardMenuController {
         this.clientService = clientService;
     }
 
+//    @GetMapping
+//    protected String adminCardMenuMakeGet(HttpServletRequest request) throws ServletException, IOException {
+//       return new AdminCardMenuService().adminCardMenuMake(request, cardService, clientService);
+//    }
+//
+//    @PostMapping
+//    protected String adminCardMenuMakePost(HttpServletRequest request) throws ServletException, IOException {
+//        return new AdminCardMenuService().adminCardMenuMake(request, cardService, clientService);
+//    }
     @GetMapping
-    protected String adminCardMenuMakeGet(HttpServletRequest request) throws ServletException, IOException {
-       return new AdminCardMenuService().adminCardMenuMake(request, cardService, clientService);
-    }
+    public String adminCardMenuGet() { return "adminCardsMenu"; }
 
     @PostMapping
-    protected String adminCardMenuMakePost(HttpServletRequest request) throws ServletException, IOException {
-        return new AdminCardMenuService().adminCardMenuMake(request, cardService, clientService);
+    public String adminCardMenuPost() {return "adminCardsMenu"; }
+
+    @GetMapping("/requestsToUnlockCards")
+    public String adminRequestsToUnlockCardsGet(HttpServletRequest request) {
+        return new AdminUnlockCardsService().unlockCard(request, cardService);
+    }
+    @PostMapping("/requestsToUnlockCards")
+    public String adminRequestsToUnlockCardsPost(HttpServletRequest request) {
+        return new AdminUnlockCardsService().unlockCard(request, cardService);
     }
 }
