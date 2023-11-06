c98df47b72e5bcb8afd1e92b18f8b50a02088c0f
Tyeryekhova Iryna <iro4kaforyou@gmail.com>
2023-11-06 11:38:23 +0200

    fix problem in AdminStatusChangeService.java with get CardAccount's data

--- src/main/java/org/example/models/AdminStatusChangeService.java
+++ src/main/java/org/example/models/AdminStatusChangeService.java
@@ -3,6 +3,7 @@ package org.example.models;
 import org.example.dbServices.DBCardService;
 import org.example.dbServices.Statuses;
 import org.example.entities.CardAccount;
+import org.example.services.Service;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -12,15 +13,17 @@ public class AdminStatusChangeService {
 
     public String cardStatusChange(HttpServletRequest request, DBCardService cardService) {
         HttpSession session = request.getSession();
-        CardAccount cardAccount = (CardAccount)session.getAttribute("cardAdminChangeStatus");
-
-        if (cardAccount == null) return "redirect:/infoMessage?message=error";
+        Service service = new Service();
+        String cardNumber =  service.getParameter(session, request,"card");
+        CardAccount cardAccount = cardService.getCardInfo(cardNumber);
+        if (cardAccount == null) return "redirect:/infoMessage?message=error&adminPage=true";
         else if ((cardAccount.getValidityPeriod()).compareTo(LocalDate.now().toString()) < 0) {
             return "redirect:/infoMessage?message=sendRequestAdminErrValidityPeriod&adminPage=true";
         } else {
             int updateResult = cardService.cardStatusChange(cardAccount.getNumber(), Statuses.UNBLOCK);
-            int sendRequest = cardService.requestAdminChange(cardAccount.getId(), Statuses.DONE);
-
+            int sendRequest = cardService.requestAdminChange(cardAccount.getNumber(), Statuses.DONE);
+            System.out.println(updateResult);
+            System.out.println(sendRequest);
             if (updateResult == 1 && sendRequest == 1) return "redirect:/infoMessage?message=status&adminPage=true";
             else return "redirect:/infoMessage?message=sendRequestAdminErr&adminPage=true";
         }
